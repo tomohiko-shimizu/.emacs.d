5b418f2f38a396d8a650e4743b034ffb5a4598fd
tomohiko shimizu <tomo7105@gmail.com>
2013-06-09 00:09:13 +0900

    OSがwindowsの時だけサーバを起動する用に修正 また，diredのファイル表示をShift_JISに変更

--- init.el
+++ init.el
@@ -4,3 +4,5 @@
 (setq default-buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
+(when (eq system-type 'windows-nt)
+  (setq default-file-name-coding-system 'shift_jis))
